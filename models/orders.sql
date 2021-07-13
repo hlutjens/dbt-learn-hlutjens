e2179753c6e581986488c7a09f534da4511399f7
Heinrich.Lutjens@nasdaq.com <Heinrich.Lutjens@nasdaq.com>
2021-07-13 20:49:17 +0000

    adding payments data

---
+++ models/orders.sql
@@ -0,0 +1,21 @@
+with orders as (
+    select * from {{ ref('stg_orders') }}
+),
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        sum(payments.payment_amount) as amount
+    from orders
+    join payments on orders.order_id = payments.order_id
+    and payments.payment_status = 'success'
+    group by orders.order_id,orders.customer_id,orders.order_date
+
+)
+
+select * from final
